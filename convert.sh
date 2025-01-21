f91dc1f230c677525981523eb8342e35fcad5112
Fioren <102145692+FiorenMas@users.noreply.github.com>
2025-01-22 00:08:33 +0700

    Update

--- convert.sh
+++ convert.sh
@@ -20,7 +20,7 @@ while read -r line; do
         done
         file="$suffix$file"
       fi
-      wget -q -U "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0" "$url" -O "download/$file"
+      wget -q --header="User-Agent: Mozilla/5.0 (Android 14; Mobile; rv:134.0) Gecko/134.0 Firefox/134.0" --header="Content-Type: application/octet-stream" --header="Accept-Language: en-US,en;q=0.9" --header="Connection: keep-alive" --header="Upgrade-Insecure-Requests: 1" --header="Cache-Control: max-age=0" --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" --keep-session-cookies --timeout=30 "$url" -O "download/$file"
       sed -i "s|$url|https://raw.githubusercontent.com/$repository/release/release/$file|g" List
     fi
   fi
@@ -37,7 +37,7 @@ done
 for file in meta/*.meta.js; do
   base=$(basename "$file" .meta.js)
   sed -i '/^\/\/ @name:/ { /^\/\/ @name:en/!d }' "$file"
-  sed -i '/^\/\/ @description:/d' "$file"
+  sed -i '/^\/\/ @description/d' "$file"
   sed -i "s|// @downloadURL .*|// @downloadURL https://raw.githubusercontent.com/$repository/release/release/$base.user.js|" "$file"
   sed -i "s|// @updateURL .*|// @updateURL https://raw.githubusercontent.com/$repository/release/release/$base.meta.js|" "$file"
 done
@@ -49,22 +49,28 @@ function compile_js() {
   eval terser --compress --mangle --comments false --parse bare_returns --output js/$base.js -- $file
 }
 export -f compile_js
-parallel -j 8 compile_js ::: pr-js/*.js
+parallel -j 16 compile_js ::: pr-js/*.js
 
-# Merge meta files and js files after compile
+# Merge meta files and js files, check errors
 for file in js/*.js; do
   base=$(basename "$file" .js)
-  cat "meta/$base.meta.js" "js/$base.js" > "release/$base.user.js"
+  userscript_name=$(grep -m 1 -oP '^// @name:en\s+\K.*' "meta/$base.meta.js" || \
+                   grep -m 1 -oP '^// @name\s+\K.*' "meta/$base.meta.js")
+  userscript_name=$(echo "$userscript_name" | tr -d '\r\n')
+  if [[ -z "$userscript_name" ]]; then
+    echo -e "\e[31mError: Missing \"$base.meta.js\"\e[0m"
+    continue
+  fi
+  js_file_content=$(cat "js/$base.js")
+  js_file_length=${#js_file_content}
+  if [[ $js_file_length -lt 100 ]]; then
+    echo -e "\e[31mFailed to convert userscript: \"$userscript_name\"\e[0m"
+  else
+    cat "meta/$base.meta.js" "js/$base.js" > "release/$base.user.js"
+    echo -e "Completed to convert userscript: \"$userscript_name\""
+  fi
 done
 
 #Releasing
 mv meta/*.meta.js release/
 mv List release/README.md
-
-#Checking convert errors:
-for meta in release/*.meta.js; do
-  base=${meta%.meta.js}
-  if [ -z "$base.user.js" ]; then
-     echo -e "\e[31m[-] Failed to convert user file $base.user.js\e[0m"
-  fi
-done
